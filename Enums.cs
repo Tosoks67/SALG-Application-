9575db2d71a4d882ca5128a0a5128f887374a83f
Tosoks67 <efkar.2010@outlook.com>
2025-10-12 14:43:52 +0200

    Remove Short ranks; add DefaultFormat and NotesMessage

--- Enums.cs
+++ Enums.cs
@@ -1,5 +1,8 @@
 ﻿namespace SALG__Application_
 {
+    /// <summary>
+    /// DoS ranks
+    /// </summary>
     public enum Rank
     {
         None,
@@ -15,19 +18,9 @@
         Assistant_Director_of_Security
     }
 
-    public enum RankShort
-    {
-        PVT = Rank.Private,
-        LCPL = Rank.Lance_Corporal,
-        CPL = Rank.Corporal,
-        SGT = Rank.Sergeant,
-        SSGT = Rank.Staff_Sergeant,
-        LT = Rank.Lieutenant,
-        MAJ = Rank.Major,
-        COL = Rank.Colonel,
-        ADoS = Rank.Assistant_Director_of_Security
-    }
-
+    /// <summary>
+    /// TRT ranks
+    /// </summary>
     public enum TRTRank
     {
         None,
@@ -38,10 +31,18 @@
         TRT_Captain
     }
 
-    public enum TRTRankShort
+    public static class Defaults
     {
-        SGT = TRTRank.TRT_Sergeant,
-        FSGT = TRTRank.TRT_First_Sergeant,
-        CPT = TRTRank.TRT_Captain
+        public const string DefaultFormat =
+            "**Username: **$(username)" +
+            "\r\n**Rank: **$(rank)" +
+            "\r\n**Start Time: **$(start_time)" +
+            "\r\n**End time: **$(end_time)" +
+            "\r\n**Total time on-site: **$(time_on_site) minutes" +
+            "\r\n**Total time: **$(total_time) minutes" +
+            "\r\n$(q)-# Quota: $(quota_done) / $(current_quota)" +
+            "\r\n__**Evidence: **__" +
+            "\r\n$(n)**Note(s): **$(notes)";
+        public const string NotesMessage = "\n\nDo know that only the first line is loaded.";
     }
 }
