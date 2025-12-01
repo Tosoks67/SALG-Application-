c7569c0fa955221346649ab519c9c498bb610e54
Tosoks67 <126388095+Tosoks67@users.noreply.github.com>
2025-12-01 22:29:43 +0100

    minor fixes to make code cleaner

--- LicWindow.xaml.cs
+++ LicWindow.xaml.cs
@@ -1,36 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
-namespace SALG__Application_
+namespace SALG__Application_;
+
+public partial class LicWindow
 {
-    /// <summary>
-    /// Logika interakcji dla klasy License.xaml
-    /// </summary>
-    public partial class LicWindow : Window
+    public LicWindow()
     {
-        public LicWindow()
-        {
-            InitializeComponent();
-            var assembly = Assembly.GetExecutingAssembly();
-            using Stream? stream = assembly.GetManifestResourceStream("SALG__Application_.LICENSE.txt");
-            if (stream != null)
-            {
-                using StreamReader reader = new(stream);
-                txtLicense.Text = reader.ReadToEnd();
-            }
-        }
+        InitializeComponent();
+        var assembly = Assembly.GetExecutingAssembly();
+        using var stream = assembly.GetManifestResourceStream("SALG__Application_.LICENSE.txt");
+        if (stream is null) return;
+        using StreamReader reader = new(stream);
+        txtLicense.Text = reader.ReadToEnd();
     }
 }
