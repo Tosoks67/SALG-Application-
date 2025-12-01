f95792586791b245c19c77033c4094965012c1b6
Tosoks67 <126388095+Tosoks67@users.noreply.github.com>
2025-12-01 22:37:46 +0100

    new class to be used in Prefs

---
+++ Prefix.cs
@@ -0,0 +1,11 @@
+﻿namespace SALG__Application_;
+
+public class Prefix
+{
+    public string Text { get; set; } = "";
+    public bool Enabled { get; set; } = true;
+
+    public override string ToString() =>
+        $"Text: {Text}" +
+        $"\nEnabled: {Enabled}";
+}
