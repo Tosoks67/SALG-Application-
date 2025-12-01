a0163ad49a7c908d9cc88a91af6f18169febb363
Tosoks67 <126388095+Tosoks67@users.noreply.github.com>
2025-12-01 22:44:35 +0100

    almost entirely rewrite user data

--- UserData.cs
+++ UserData.cs
@@ -1,44 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static SALG__Application_.Functions;
 
-namespace SALG__Application_
+namespace SALG__Application_;
+
+/// <summary>
+/// Class representing user data (username, rank, quota done, total time, etc.)
+/// </summary>
+public class UserData
 {
-    /// <summary>
-    /// Class representing user data (username, rank, quota done, total time, etc.)
-    /// </summary>
-    public class UserData
-    {
-        private string _rankString = "None";
-        public string Username { get; set; } = "John Doe";
-        public string RankString
-        {
-            get => _rankString;
-            set
-            {
-                _rankString = value;
-                UpdateRanks();
-            }
-        }
-        public Rank Rank { get; private set; } = Rank.None;
-        public TRTRank TRTRank { get; private set; } = TRTRank.None;
-        public int QuotaDone { get; set; } = 0;
-        public int TotalTime { get; set; } = 0;
-        public int CurrentQuota { get; set; } = 0;
-        public bool ShowQuotaDone { get; set; } = true;
+    public string Username { get; set; } = "John Doe";
+    public string RankString { get; set; } = "";
+    public DoS_Rank DoS => TryParseRankOrDefault<DoS_Rank>();
+    public TRT_Rank Trt => TryParseRankOrDefault<TRT_Rank>();
+    public Alpha1_Rank Alpha1 => TryParseRankOrDefault<Alpha1_Rank>();
+    public Epsilon11_Rank Epsilon11 => TryParseRankOrDefault<Epsilon11_Rank>();
+    public Nu7_Rank Nu7 => TryParseRankOrDefault<Nu7_Rank>();
+    public int QuotaDone { get; set; }
+    public int TotalTime { get; set; }
+    public int CurrentQuota { get; set; }
+    public bool ShowQuotaDone { get; set; } = true;
 
-        /// <summary>
-        /// Is fired each time RankString is set, updates the Rank and TRTRank properties accordingly.
-        /// </summary>
-        private void UpdateRanks()
-        {
-            if (!Enum.TryParse(RankString.Replace(' ', '_'), out Rank r)) r = default;
-            if (!Enum.TryParse(RankString.Replace(' ', '_'), out TRTRank tR)) tR = default;
-            Rank = r;
-            TRTRank = tR;
-        }
-    }
+    private T TryParseRankOrDefault<T>() where T : struct, Enum => Enum.TryParse(RankString.Replace(' ', '_'), out T r) ? r : default;
+
+    public override string ToString() =>
+        $"Username: {Username}" +
+        $"\nRank String: {RankString}" +
+        $"\nDoS: {DoS}" +
+        $"\nTRT: {Trt}" +
+        $"\nAlpha-1: {Alpha1}" +
+        $"\nEpsilon-11: {Epsilon11}" +
+        $"\nNu-7: {Nu7}" +
+        $"\nQuota Done: {QuotaDone}" +
+        $"\nTotal Time: {TotalTime}" +
+        $"\nCurrent Quota: {CurrentQuota}" +
+        $"\nShow Quota Done: {ShowQuotaDone}";
 }
