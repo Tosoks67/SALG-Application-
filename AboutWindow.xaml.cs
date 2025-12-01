bd978287a0dfefab0a0bb700c041379c652777fc
Tosoks67 <126388095+Tosoks67@users.noreply.github.com>
2025-12-01 22:21:33 +0100

    tidying up
    removed unnecessary libs
    moved to file scope namespaces

--- AboutWindow.xaml.cs
+++ AboutWindow.xaml.cs
@@ -1,34 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
-namespace SALG__Application_
+namespace SALG__Application_;
+
+public partial class AboutWindow : Window
 {
-    /// <summary>
-    /// Logika interakcji dla klasy AboutWindow.xaml
-    /// </summary>
-    public partial class AboutWindow : Window
+    public AboutWindow()
     {
-        public AboutWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
 
-        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
-        {
-            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
-            e.Handled = true;
-        }
+    private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+    {
+        Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
+        e.Handled = true;
     }
 }
