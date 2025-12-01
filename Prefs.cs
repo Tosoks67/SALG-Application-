b11e88b9dc7cc4a3615dc71274280307b9702936
Tosoks67 <126388095+Tosoks67@users.noreply.github.com>
2025-12-01 22:40:29 +0100

    update to match new departments, 2 fixes
    file scoped namespace, remove unnecessary `using`s

--- Prefs.cs
+++ Prefs.cs
@@ -1,14 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace SALG__Application_;
 
-namespace SALG__Application_
+public class Prefs
 {
-    public class Prefs
-    {
-        public bool DarkMode { get; set; } = false;
-        public bool TRT { get; set; } = false;
-    }
+    public bool DarkMode { get; set; }
+    public Department Department { get; set; } = Department.DoS;
+    public Prefix DoSPrefix { get; } = new() { Text = "Security", Enabled = true };
+    public Prefix TrtPrefix { get; } = new() { Text = "Tactical Response", Enabled = true };
+    public Prefix Alpha1Prefix { get; } = new() { Text = "Alpha-1", Enabled = true };
+    public Prefix Epsilon11Prefix { get; } = new() { Text = "Epsilon-11", Enabled = true };
+    public Prefix Nu7Prefix { get; } = new() { Text = "Nu-7", Enabled = true };
+
+    public override string ToString() =>
+        $"Dark Mode: {DarkMode}" +
+        $"\nDepartment: {Department}" +
+        $"\n\nDoS prefix: \n{DoSPrefix}" +
+        $"\n\nTRT prefix: \n{TrtPrefix}" +
+        $"\n\nAlpha-1 prefix: \n{Alpha1Prefix}" +
+        $"\n\nEpsilon-11 prefix: \n{Epsilon11Prefix}" +
+        $"\n\nNu-7 prefix: \n{Nu7Prefix}";
 }
